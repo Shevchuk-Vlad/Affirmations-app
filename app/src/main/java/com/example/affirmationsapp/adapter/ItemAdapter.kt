1ea46db41bbf19a0471ae84f63bcc826c12c0744
Vlad <Vlad17.lutsk@gmail.com>
2023-12-14 16:36:24 +0200

    Modify the MainActivity

--- app/src/main/java/com/example/affirmationsapp/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmationsapp/adapter/ItemAdapter.kt
@@ -9,31 +9,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmationsapp.R
 import com.example.affirmations.model.Affirmation
 
-class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>) {
-    class ItemAdapter(
-        private val context: Context,
-        private val dataset: List<Affirmation>)
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-        : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-
-
-        class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-            val textView: TextView = view.findViewById(R.id.item_title)
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-
-            val adapterLayout = LayoutInflater.from(parent.context)
-                .inflate(R.layout.list_item, parent, false)
-
-            return ItemViewHolder(adapterLayout)
-        }
-
-        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-            val item = dataset[position]
-            holder.textView.text = context.resources.getString(item.stringResourceId)
-        }
-
-        override fun getItemCount() = dataset.size
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+    override fun getItemCount() = dataset.size
 }
